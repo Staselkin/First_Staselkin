21d10ca6a672c1162bca60a61772695afaaa1732
Staselkin <165091422+Staselkin@users.noreply.github.com>
2024-04-16 00:52:09 +0300

    Start

--- Homework3.py
+++ Homework3.py
@@ -1,4 +1,15 @@
-import datetime
-now = datetime.datetime.now()
-print(now)
+from datetime import datetime
 
+
+def string_to_date(date_string):
+    return datetime.strptime(date_string, '%Y.%m.%d').date()
+date_string = "2024.01.01"
+converted_date = string_to_date(date_string)
+print (converted_date)
+
+
+def date_to_string(date):
+    return date.strftime('%Y.%m.%d')
+date = datetime(2024,1,1).date()
+newdate = date_to_string(date)
+print (newdate)
