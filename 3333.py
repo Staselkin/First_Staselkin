3d93e1f71af4fe54d414b740fa5ff0f1fd4ac196
Staselkin <165091422+Staselkin@users.noreply.github.com>
2024-04-18 00:36:39 +0300

    DONE_18042024

--- 3333.py
+++ 3333.py
@@ -3,19 +3,18 @@ from datetime import datetime, timedelta
 def string_to_date(date_string):
     return datetime.strptime(date_string, "%Y.%m.%d").date()
 
-start_date= string_to_date("2024.03.26").weekday()
-
 def find_next_weekday(start_date, weekday):
        
-    if (weekday-start_date)<=0:
-        days_aheads=(weekday-start_date+7)
+    if (weekday-start_date.weekday())<=0:
+        days_aheads=(weekday-start_date.weekday()+7)
     else:
-        days_aheads=(weekday-start_date)
+        days_aheads=(weekday-start_date.weekday())
         
     return start_date + timedelta(days=days_aheads)
-    
+
+start_date= string_to_date("2024.03.26")
 next_monday= find_next_weekday(start_date, 0)
 print(next_monday)
 next_friday = find_next_weekday(start_date, 4) 
-print(next_friday)    
+print(next_friday)   
 
