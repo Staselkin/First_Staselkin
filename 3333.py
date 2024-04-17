278be0876b6330ef331ba44c437f7629d6d130ee
Staselkin <165091422+Staselkin@users.noreply.github.com>
2024-04-17 22:21:51 +0300

    2_17042024

--- 3333.py
+++ 3333.py
@@ -1,5 +1,10 @@
 from datetime import datetime, timedelta
 
+start_date = datetime.now().date()
+print(start_date)
+current_date = datetime.now()
+day_of_week = current_date.weekday()
+print(day_of_week)
 
 def string_to_date(date_string):
     return datetime.strptime(date_string, "%Y.%m.%d").date()
