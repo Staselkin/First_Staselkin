55a16d1cad8c314d24ba780ddd2d053c50ec93fb
Staselkin <165091422+Staselkin@users.noreply.github.com>
2024-04-21 19:48:04 +0300

    HomeWork_1_done

--- Home_Work3.py
+++ Home_Work3.py
@@ -1,18 +1,13 @@
 from datetime import datetime, timedelta
 
-def string_to_date(date_string):
-    return datetime.strptime(date_string, "%Y.%m.%d").date()
+def get_days_from_today(date):
+    date = datetime.strptime(date, "%Y.%m.%d").date()
+    current_date = datetime.today().date()
+    diff = (current_date-date)
+    return diff.days
+date = ("2024.10.09")
+print(get_days_from_today(date))
 
-current_date = datetime.today().date()
-start_date= string_to_date("2024.03.26")
-print (current_date)
-print (start_date)
-diff = (current_date-start_date)
-print ("Різниця у днях:", diff.days)
 
-#def get_days_from_today(current_date, start_date):
- #   return current_date-start_date
-
-#print (get_days_from_today)
 
 
