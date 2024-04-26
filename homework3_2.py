30360c2092e3bf411dd77240ce407a6e2fde14d7
Staselkin <165091422+Staselkin@users.noreply.github.com>
2024-04-26 23:15:29 +0300

    26042024

--- homework3_2.py
+++ homework3_2.py
@@ -6,10 +6,8 @@ max=49
 quantity=6
 
 def get_numbers_ticket(min, max, quantity):
-    numbers = random.sample(range(min, max), quantity)
+    numbers = random.sample(range(min, max+1), quantity)
     return numbers
 
 ticket = sorted(list(get_numbers_ticket(min, max, quantity)))
 print("Лотерейний квиток:", ticket)
-
-#/////
