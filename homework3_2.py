a2f5358076b998b1c1dfc2df4d070ef36053dac3
Staselkin <165091422+Staselkin@users.noreply.github.com>
2024-04-26 23:24:45 +0300

    26042024

--- homework3_2.py
+++ homework3_2.py
@@ -6,6 +6,21 @@ max=49
 quantity=6
 
 def get_numbers_ticket(min, max, quantity):
+    if min_num < 1:
+        return []
+
+    if max_num > 1000:
+        return []
+
+    if min_num >= max_num:
+        return []
+
+    if quantity < 1:
+        return []
+
+    if quantity > (max_num - min_num + 1):
+        return []
+
     numbers = random.sample(range(min, max+1), quantity)
     return numbers
 
